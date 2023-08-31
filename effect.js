7e8cc342f4b2e687ea123b0b3ef46d1bc0af4a2a
DzakirizaDF <ddf7802@gmail.com>
2023-08-31 16:04:25 +0700

    update banyak(tailwindjs dll)

--- effect.js
+++ effect.js
@@ -6,3 +6,74 @@ document.addEventListener('scroll', () =>{
         nav.classList.remove('scrolled');
     }
 })
+/*
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
+
+// set the default active slide to the first one
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// change slide with the prev/next button
+function moveSlide(moveStep) {
+    showSlide(slideIndex += moveStep);
+}
+
+// change slide with the dots
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName('dot');
+    
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    // hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hidden');
+    }
+
+    // remove active status from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('bg-yellow-500');
+        dots[i].classList.add('bg-green-600');
+    }
+
+    // show the active slide
+    slides[slideIndex - 1].classList.remove('hidden');
+
+    // highlight the active dot
+    dots[slideIndex - 1].classList.remove('bg-green-600');
+    dots[slideIndex - 1].classList.add('bg-yellow-500');
+}
+*/
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
